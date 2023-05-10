91617668f482d46a71abf61cfbac35d298931784
Diogo Sales <diogosales.ads@gmail.com>
2023-05-10 19:39:16 -0300

    Create and render the Card component with the necessary props

---
+++ src/components/Card.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardImage,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <div>
+        <p>{cardName}</p>
+        <img src={ cardImage } alt={ cardName } />
+        <p>{cardDescription}</p>
+        <p>{cardAttr1}</p>
+        <p>{cardAttr2}</p>
+        <p>{cardAttr3}</p>
+        <p>{cardRare}</p>
+        {cardTrunfo === true ? <p>Super Trunfo</p> : ''}
+
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+}.isRequired;
